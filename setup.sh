9eec4790517ef925e4d3038fb89c72bbbc403d2b
fadharpra <75076391+fadharpra@users.noreply.github.com>
2021-11-22 18:49:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,55 +1,70 @@
 #!/bin/sh
 ############################################################
-# Kubernetes Install Script for Debian 9 (Stretch)
-# 
-# run as sudo 
+# Kubernetes Install Script for CentOS 7          
+#
+# run as sudo
 ############################################################
 
-# determine if we run as sudo
-userid="${SUDO_USER:-$USER}"
-if [ "$userid" == 'root' ]
-  then 
-    echo "Please run the setup as sudo and not as root!"
-    exit 1
-fi
-if [ "$EUID" -ne 0 ]
-  then 
-    echo "Please run setup as sudo!"
-    exit 1
-fi
+echo "#############################################"
+echo " Disabled Selinux "
+echo "#############################################"
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 
 echo "#############################################"
-echo " adding repositories..."
+echo " Enable br_netfilter Kernel Module "
 echo "#############################################"
-apt-get update
-apt-get install -y apt-transport-https ca-certificates ne curl gnupg2 software-properties-common
+modprobe br_netfilter
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 
-# Add docker repositry
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository \
-          "deb [arch=amd64] https://download.docker.com/linux/debian \
-           $(lsb_release -cs) \
-           stable"
-           
-# Add kubernetes repository           
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+echo "#############################################"
+echo " Disable SWAP "
+echo "#############################################"
+swapoff -a
+
+echo "#############################################"
+echo " Install Docker CE "
+echo "#############################################"
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
+ 
+echo "#############################################"
+echo " adding repositories kubernetes "
+echo "#############################################"
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+yum install -y kubelet kubeadm kubectl
 
 echo "#############################################"
-echo " installing docker and kubernetes...."
+echo " Start and Enabled Kubernetes Service "
 echo "#############################################"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io kubelet kubeadm kubectl
+systemctl start docker && systemctl enable docker 
+systemctl start kubelet && systemctl enable kubelet
+
+echo "#############################################"
+echo " Change the cgroup-driver "
+echo "#############################################"
+docker info | grep -i cgroup
+sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+systemctl restart kubelet
 
 #####################################################################################
-# Kubernetes is now installed. To setup a new kubernetes cluster with a master node 
+# Kubernetes is now installed. To setup a new kubernetes cluster with a master node
 # run:
-#  $ kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=[YOUR-NODE-IP-ADDRESS]
+#  $ kubeadm init --apiserver-advertise-address=[YOUR-NODE-IP-ADDRESS] --pod-network-cidr=10.244.0.0/16
 #
 # This command will setup a new cluster. Follow the instructions of the output.
 # The output will show also the command how to join a worker node.
-# You can use this script also to install a worker node. 
+# You can use this script also to install a worker node.
 #####################################################################################
+
